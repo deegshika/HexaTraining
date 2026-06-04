db2becf87fa625654db0c3bffeb23f4e2115c65f
Deegshika <130476444+deegshika@users.noreply.github.com>
2026-06-04 16:55:01 +0530

    Create data_types.py

---
+++ June 4/data_types.py
@@ -0,0 +1,17 @@
+print("Hello World !!")
+
+name="Rahul Sharma"
+age=28
+salary=75000.20
+is_active=True
+
+print(name)
+print(age)
+print(salary)
+print(is_active)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_active))
+
